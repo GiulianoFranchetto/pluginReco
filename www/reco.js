ec3d18ee883090b4d8af3028c1efe9a81ea100e7
GiulianoFranchetto <franchetto.giuliano@gmail.com>
2014-10-17 16:56:16 +0200

    a

--- www/reco.js
+++ www/reco.js
@@ -1,53 +1,58 @@
-var cordova = require('cordova'),
-    exec = require('cordova/exec');
+var cordova = require('cordova');
 
-var Recognizer = function() {
-  
-    this.channels = {
-      recoError:cordova.addWindowEventHandler("recoError"),
-      recoNewMessage:cordova.addWindowEventHandler("recoNewMessage"),
-      recoSetupCompleted:cordova.addWindowEventHandler("recoSetupCompleted")
-    };
-};
-   
-Recognizer.prototype.message = function (info) {
-    if (info)  
-        cordova.fireWindowEvent("recoNewMessage", info.message);
-};
+cordova.define("cordova/plugin/ipaddress", 
+    function(require, exports, module) {
+        var exec = require('cordova/exec');
 
-Recognizer.prototype.setupOK = function () {
-        cordova.fireWindowEvent("recoSetupCompleted", info.message);
-};
+        var Recognizer = function() {
+          
+            this.channels = {
+              recoError:cordova.addWindowEventHandler("recoError"),
+              recoNewMessage:cordova.addWindowEventHandler("recoNewMessage"),
+              recoSetupCompleted:cordova.addWindowEventHandler("recoSetupCompleted")
+            };
+        };
+           
+        Recognizer.prototype.message = function (info) {
+            if (info)  
+                cordova.fireWindowEvent("recoNewMessage", info.message);
+        };
 
-Recognizer.prototype.error = function(info) {
-    cordova.fireWindowEvent("recoError", info.message);
-};
+        Recognizer.prototype.setupOK = function () {
+                cordova.fireWindowEvent("recoSetupCompleted", info.message);
+        };
 
-Recognizer.prototype.setupRecognizer = function(acoustic, dictionnary){
-      exec(recognizer.setupOK, recognizer.error, "Recognizer", "setupRecognizer", [acoustic, dictionnary]);
+        Recognizer.prototype.error = function(info) {
+            cordova.fireWindowEvent("recoError", info.message);
+        };
 
-};
+        Recognizer.prototype.setupRecognizer = function(acoustic, dictionnary){
+              exec(recognizer.setupOK, recognizer.error, "Recognizer", "setupRecognizer", [acoustic, dictionnary]);
 
-Recognizer.prototype.setupGrammar = function(name, path){
-      exec(recognizer.setupOK, recognizer.error, "Recognizer", "setupGrammar", [name, path]);
+        };
 
-};
+        Recognizer.prototype.setupGrammar = function(name, path){
+              exec(recognizer.setupOK, recognizer.error, "Recognizer", "setupGrammar", [name, path]);
 
-Recognizer.prototype.setupKeyphrase = function(name, phrase){
-      exec(recognizer.setupOK, recognizer.error, "Recognizer", "setupKeyphrase", [name, phrase]);
+        };
 
-};
+        Recognizer.prototype.setupKeyphrase = function(name, phrase){
+              exec(recognizer.setupOK, recognizer.error, "Recognizer", "setupKeyphrase", [name, phrase]);
 
-Recognizer.prototype.startListening = function(name){
-      exec(recognizer.message, recognizer.error, "Recognizer", "startListening", [name]);
+        };
 
-};
+        Recognizer.prototype.startListening = function(name){
+              exec(recognizer.message, recognizer.error, "Recognizer", "startListening", [name]);
 
-Recognizer.prototype.stopListening = function(){
-      exec(recognizer.setupOK, recognizer.error, "Recognizer", "stopListening", []);
+        };
 
-};
+        Recognizer.prototype.stopListening = function(){
+              exec(recognizer.setupOK, recognizer.error, "Recognizer", "stopListening", []);
 
-var recognizer = new Recognizer();
+        };
 
-module.exports = recognizer;
+        var recognizer = new Recognizer();
+
+        module.exports = recognizer;
+    }
+);
